87a696513eb17da7a11e7f1ff00556b24bde8f48
Hal305 <halvork@live.no>
2024-06-28 15:23:01 +0200

    Tweaks

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 
 using json = nlohmann::json;
 
-std::string readURL(std::string filename);
 const std::string file = "config.ini";
 
 //https://stackoverflow.com/a/9786295
@@ -33,12 +32,15 @@ std::string readURL(std::string filename)
 
 int main(void)
 {
+    curl_global_init(CURL_GLOBAL_ALL);
+
     CURL* curl;
     CURLcode res;
     std::string readBuffer;
 
     curl = curl_easy_init();
-    if (curl) {
+    if (curl) 
+    {
         readBuffer.clear();
         //https://curl.se/libcurl/c/curl_easy_setopt.html
         curl_easy_setopt(curl, CURLOPT_URL, readURL(file).c_str());
@@ -51,22 +53,23 @@ int main(void)
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
-
-        json j = json::parse(readBuffer);
-
-        //std::cout << j.dump(2) << std::endl; 
-
-        for (auto& el : j.items())
+        else 
         {
-            std::cout << el.key() << "\n";
-            std::cout << el.value().find("title").value().front() << "\n";
-            std::cout << el.value().find("date").value() << "\n";
-            std::cout << el.value().find("id").value() << "\n";
-        }
+            json j = json::parse(readBuffer);
 
+            for (auto& el : j.items())
+            {
+                std::cout << el.key() << "\n";
+                std::cout << el.value().find("title").value().front() << "\n";
+                std::cout << el.value().find("date").value() << "\n";
+                std::cout << el.value().find("id").value() << "\n";
+            }
+        }
+        
         /* always cleanup */
         curl_easy_cleanup(curl);
 
     }
+    curl_global_cleanup();
     return 0;
 }
